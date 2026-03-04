5b7100f2ccde7eaee2c756e6df078d96b1631a57
Gabriel Dias <gabriiel.diasp@gmail.com>
2026-03-04 20:39:29 -0300

    gerenciamento

---
+++ HubComercio/Controllers/GestaoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HubComercio.Controllers
+{
+    [Authorize]
+    public class GestaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
